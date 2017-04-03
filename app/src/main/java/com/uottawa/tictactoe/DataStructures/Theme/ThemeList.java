c630def6c929b7d5f748f421d3adedf82564642e
alanrossx2 <alanrossx2@hotmail.com>
2017-04-02 21:59:35 -0400

    Added new themes

--- app/src/main/java/com/uottawa/tictactoe/DataStructures/Theme/ThemeList.java
+++ app/src/main/java/com/uottawa/tictactoe/DataStructures/Theme/ThemeList.java
@@ -12,32 +12,36 @@ public class ThemeList {
     private int imageID;
 
     public ThemeList(){
-        // Purple
         int theme1backgroundId = R.drawable.background1;
-        int theme1Color = Color.rgb(255, 36, 226);
-        int theme1SampleImage = theme1backgroundId;
+        int theme1Color = Color.rgb(255, 36, 226); //Purple
+        int theme1SampleImage = R.drawable.background1sample;
         Theme theme1 = new Theme(theme1SampleImage, theme1backgroundId, theme1Color);
 
         int theme2backgroundId = R.drawable.background2;
         int theme2Color = Color.RED;
-        int theme2SampleImage = theme2backgroundId;
+        int theme2SampleImage = R.drawable.background2sample;
         Theme theme2 = new Theme(theme2SampleImage, theme2backgroundId, theme2Color);
 
         int theme3backgroundId = R.drawable.background3;
-        int theme3Color = Color.BLUE;
-        int theme3SampleImage = theme3backgroundId;
+        int theme3Color = Color.rgb(128,128,128); // Gray
+        int theme3SampleImage = R.drawable.background3sample;
         Theme theme3 = new Theme(theme3SampleImage, theme3backgroundId, theme3Color);
+
+        int theme4backgroundId = R.drawable.background4;
+        int theme4Color = Color.rgb(102,153,0); // holo_green_light
+        int theme4SampleImage = R.drawable.background4sample;
+        Theme theme4 = new Theme(theme4SampleImage, theme4backgroundId, theme4Color);
 
         theme_list = new ArrayList<Theme>();
         theme_list.add(theme1);
         theme_list.add(theme2);
         theme_list.add(theme3);
+        theme_list.add(theme4);
     }
 
     public ArrayList<Theme> getThemeList(){
         return theme_list;
     }
-
 
     public void setTheme(int theme_position){
         position = theme_position;
