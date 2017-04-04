ea8fde098c572186b4a4b61b7c43200bcb69d2e0
alanrossx2 <alanrossx2@hotmail.com>
2017-04-03 21:24:50 -0400

    Only portrait mode can be used now

--- app/src/main/java/com/uottawa/tictactoe/Activity/BaseActivity.java
+++ app/src/main/java/com/uottawa/tictactoe/Activity/BaseActivity.java
@@ -2,6 +2,7 @@ package com.uottawa.tictactoe.Activity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.ActivityInfo;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
 import android.media.MediaPlayer;
@@ -47,6 +48,8 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
 
         SharedPreferences settings = getSharedPreferences(keySharedPreference, MODE_PRIVATE);
         applicationSettings = new ApplicationSettings(settings);
